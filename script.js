ba1a22f2fa3470c09a7cb7b9678c1d85cc9c187a
sowmya <19wh1a0454@bvrithyderabad.edu.in>
2023-01-01 19:09:29 +0530

    written display card function

--- script.js
+++ script.js
@@ -170,7 +170,33 @@ function initialDeal() {
     }
 
     // Deal(Instantiate) 2 Dealer cards and 2 Player cards
-
+    if (deck.length() < 7) {
+        deck.reset();
+        deck.shuffle();
+      }
+      card1 = new Card(deck.deal());
+      card2 = new Card(deck.deal());
+      playerCard1 = new Card(deck.deal());
+      playerCard2 = new Card(deck.deal());
+      card1.displayCard("card1", !0);
+      card2.displayCard("card2", !1);
+      playerCard1.displayCard("playerCard1", !0);
+      playerCard2.displayCard("playerCard2", !0);
+      card1.value = 10 < card1.value ? 10 : card1.value;
+      card2.value = 10 < card2.value ? 10 : card2.value;
+      playerCard1.value = 10 < playerCard1.value ? 10 : playerCard1.value;
+      playerCard2.value = 10 < playerCard2.value ? 10 : playerCard2.value;
+      21 === (playerTotal = playerCard1.value + playerCard2.value) &&
+        cuteAlert({
+          type: "success",
+          title: "Superb!!!",
+          message: "Blackjacked !!!",
+          buttonText: "Wohoo !!!",
+          img: "success.svg",
+        }).then(() => {
+          location.reload();
+        });
+    
     // write your code here
 
 
@@ -206,6 +232,27 @@ function initialDeal() {
  * If the Player stands with his cards - the Dealer has to flip his closed card and determine who wins the game
  */
 function stand() {
+    card2.flip();
+  dealerTotal = card1.value + card2.value;
+  (playerTotal >= dealerTotal
+    ? cuteAlert({
+        type: "success",
+        title: "Congratualtions !!!",
+        message: "You won the Game",
+        buttonText: "Yayy !",
+        img: "success.svg",
+      })
+    : cuteAlert({
+        type: "error",
+        title: "Oh No !!!",
+        message: "Dealer won the Game",
+        buttonText: "Ok :(",
+        img: "error.svg",
+      })
+  ).then(() => {
+    location.reload();
+  });
+
     // flip Dealer cards and compare
 
     // write your code here
@@ -227,7 +274,48 @@ function hit() {
     let dealButton = document.getElementById("deal");
 
     // Dealing the extra cards that the player requests
-
+    playerCard3 = new Card(deck.deal());
+    playerCard4 = new Card(deck.deal());
+    if (0 === extraCnt) {
+      playerCard3.displayCard("playerCard3", true);
+      playerCard3.value = 10 < playerCard3.value ? 10 : playerCard3.value;
+      playerTotal += playerCard3.value;
+    } else if (1 === extraCnt) {
+      playerCard4.displayCard("playerCard4", true);
+      playerCard4.value = 10 < playerCard4.value ? 10 : playerCard4.value;
+      playerTotal += playerCard4.value;
+    } else {
+      e.style.display = "none";
+      cuteAlert({
+        type: "warning",
+        title: "Sorry...",
+        message: "Max. Cards dealed",
+        buttonText: "OK",
+        img: "warning.svg",
+      });
+    }
+    if (21 < playerTotal) {
+      cuteAlert({
+        type: "error",
+        title: "Busted...",
+        message: "You lost the Game",
+        buttonText: "OK",
+        img: "error.svg",
+      }).then(() => {
+        location.reload();
+      });
+      asArray.style.display = "none";
+    } else
+      21 === playerTotal &&
+        cuteAlert({
+          type: "success",
+          title: "Superb!!!",
+          message: "Blackjacked !!!",
+          buttonText: "Wohoo !!!",
+          img: "success.svg",
+        }).then(() => {
+          location.reload();
+        });
     // write your code here
 
 
